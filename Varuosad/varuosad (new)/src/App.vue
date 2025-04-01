adfcf996a65d3690a00f5914467b8329259b69f1
kivisild <siimmarten.schmidt@gmail.com>
2025-04-01 21:51:17 +0300

    Now views data as array of strings

--- Varuosad/varuosad (new)/src/App.vue
+++ Varuosad/varuosad (new)/src/App.vue
@@ -1,8 +1,28 @@
 <script setup>
+    import Papa from 'papaparse';
+    import {ref, onMounted} from 'vue';
+    const csvData = ref([]);
+
+    onMounted(async () => {
+        const data = await fetch("/LE.txt");
+        const text = await data.text();
+        const config = {
+            header: false,
+            complete: (results) => {
+                csvData.value = results.data;
+                console.log("Parsed CSV: ", results.data)
+            },
+            error: (error) => {
+                console.error("Error parsing CSV:", error)
+            }
+        }
+        Papa.parse(text, config);
+    });
+
 
 </script>
 
 <template>
-    
+    <pre>{{csvData}}</pre>
 </template>
 
